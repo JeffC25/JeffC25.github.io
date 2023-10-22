52952a67567e8e04021d90f51577c9decbb552ed
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-22 18:22:40 -0400

    set up react + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <div className="">
+      <Navbar/>
+      Hello World
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="w-full h-20 bg-neutral-100 flex flex-row space-x-4 px-2 text-3xl items-center justify-between">
+            <div className="">Logo</div>
+            <div className="flex-grow"></div>
+            <a href="" className="">Home</a>
+            <a href="" className="">About</a>
+            <a href="" className="">Projects</a>
+            <a href="" className="">Contacts</a>
+        </nav>
+    )
+};
+
+export default Navbar;
