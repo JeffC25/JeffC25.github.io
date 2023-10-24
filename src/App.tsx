d72d0ae1ced8f7e58ca74f8df1be3c2156ae0910
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-24 01:08:59 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import Navbar from "./components/Navbar.tsx"
-import Home from "./components/Home.tsx"
-import Projects from "./components/Projects.tsx"
-import Contacts from "./components/Contacts.tsx"
-import About from "./components/About.tsx"
+// import Navbar from "./components/Navbar.tsx"
+// import Home from "./components/Home.tsx"
+// import Projects from "./components/Projects.tsx"
+// import Contacts from "./components/Contacts.tsx"
+// import About from "./components/About.tsx"
 
 function App() {
 
   return (
     <div className="w-full h-screen bg-blue-400 flex flex-col">
-      <div className="flex-grow-0 h-20">
-        <Navbar />
-      </div>
-      <div className="overflow-y-scroll h-full snap-mandatory snap-y scroll-smooth">
-        <Home />
-        <About />
-        <Projects />
-        <Contacts />
-      </div>
+      Test
+?
     </div>
   )
 }
