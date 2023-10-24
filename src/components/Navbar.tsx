d8874e19542ab82dc8193f2009d2f5eed0f5a5ee
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-24 01:45:08 -0400

    switch home and about href tags, pages deployement error disappeared?

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ const Navbar = () => {
         <nav className="w-full h-20 bg-transparent flex flex-row space-x-4 px-2 text-3xl items-center justify-between">
             <div className="">Logo</div>
             <div className="flex-grow"></div>
-            <a href="#about" className="">Home</a>
-            <a href="#home" className="">About</a>
+            <a href="#home" className="">Home</a>
+            <a href="#about" className="">About</a>
             <a href="#projects" className="">Projects</a>
             <a href="#contacts" className="">Contacts</a>
         </nav>
