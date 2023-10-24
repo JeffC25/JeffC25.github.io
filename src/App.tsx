adbdd376ff788e123c537584be95808925139bec
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-24 01:11:49 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import Navbar from "./components/Navbar.tsx"
+import Navbar from "./components/Navbar.tsx"
 // import Home from "./components/Home.tsx"
 // import Projects from "./components/Projects.tsx"
 // import Contacts from "./components/Contacts.tsx"
@@ -9,10 +9,10 @@ function App() {
   return (
     <div className="w-full h-screen bg-blue-400 flex flex-col">
       Test
-      {/* <div className="flex-grow-0 h-20">
+      <div className="flex-grow-0 h-20">
         <Navbar />
       </div>
-      <div className="overflow-y-scroll h-full snap-mandatory snap-y scroll-smooth">
+      {/* <div className="overflow-y-scroll h-full snap-mandatory snap-y scroll-smooth">
         <Home />
         <About />
         <Projects />
