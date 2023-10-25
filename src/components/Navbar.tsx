3265c69c70dc3f49b88ad8c58d9bbe79d2a7798d
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-25 00:46:50 -0400

    add hover effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <nav className="w-3/4 h-20 flex flex-row space-x-4 text-xl text-neutral-800 items-center">
-            <div className="">Under Construction !</div>
+            <div className="text-lime-500">Under Construction !</div>
             <div className="flex-grow"></div>
             <a href="#home" className="hover:text-lime-500 hover:animate-pulse">Home</a>
             <a href="#about" className="hover:text-lime-500 hover:animate-pulse">About</a>
