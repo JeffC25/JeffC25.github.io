aa69124ee8ad99228b6b1289ba9b7ca6b803597d
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-25 01:38:43 -0400

    adjust navbar responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 const Navbar = () => {
     return (
-        <nav className="w-3/4 h-20 flex flex-row space-x-4 text-xl text-neutral-800 items-center">
-            <div className="text-lime-500">Under Construction !</div>
-            <div className="flex-grow"></div>
-            <a href="#home" className="hover:text-lime-500 hover:animate-pulse">Home</a>
-            <a href="#about" className="hover:text-lime-500 hover:animate-pulse">About</a>
-            <a href="#projects" className="hover:text-lime-500 hover:animate-pulse">Projects</a>
-            <a href="#contacts" className="hover:text-lime-500 hover:animate-pulse">Contacts</a>
+        <nav className="w-3/4 h-20 flex flex-row text-xl text-neutral-800 items-center justify-between">
+            <div className="hidden sm:block ">Jeff Chen</div>
+            <div className="flex-grow-0 sm:flex-grow"></div>
+            <a href="#home" className="px-2 hover:text-lime-500 hover:animate-pulse">Home</a>
+            <a href="#about" className="px-2 hover:text-lime-500 hover:animate-pulse">About</a>
+            <a href="#projects" className="px-2 hover:text-lime-500 hover:animate-pulse">Projects</a>
+            <a href="#contacts" className="px-2 hover:text-lime-500 hover:animate-pulse">Contacts</a>
         </nav>
     )
 };
