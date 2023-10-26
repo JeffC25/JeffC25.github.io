6ff65f57bfe7c63a54195e0cc1ba53fa5c81027b
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-25 22:21:15 -0400

    adjust responsiveness for navbar and home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
-        <div id="home" className="h-full snap-start flex flex-col sm:flex-row justify-center items-center space-x-20">
-            <div className="h-1/2 w-64  flex flex-col justify-center space-y-12">
+        <div id="home" className="h-full snap-start flex flex-col sm:flex-row justify-center items-center space-y-10 sm:space-y-0 sm:space-x-20">
+            <div className="h-fit w-44 sm:w-64  flex flex-col justify-center space-y-4">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Vombatus_ursinus_-Maria_Island_National_Park.jpg/1200px-Vombatus_ursinus_-Maria_Island_National_Park.jpg" className="w-full aspect-square rounded-full object-cover"/>
 
                 <ul className="flex flex-row justify-center space-x-10">
@@ -11,18 +11,20 @@ const Home = () => {
                     <a href="https://www.linkedin.com/in/jeffrey-li-chen/" className="h-8 w-8">
                         <svg className="hover:fill-lime-500/75 hover:animate-pulse" viewBox="-5 -5 40 40" xmlns="http://www.w3.org/200/750/svg"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g><path d="M28.778 1.004h-25.56c-0.008-0-0.017-0-0.027-0-1.199 0-2.172 0.964-2.186 2.159v25.672c0.014 1.196 0.987 2.161 2.186 2.161 0.010 0 0.019-0 0.029-0h25.555c0.008 0 0.018 0 0.028 0 1.2 0 2.175-0.963 2.194-2.159l0-0.002v-25.67c-0.019-1.197-0.994-2.161-2.195-2.161-0.010 0-0.019 0-0.029 0h0.001zM9.9 26.562h-4.454v-14.311h4.454zM7.674 10.293c-1.425 0-2.579-1.155-2.579-2.579s1.155-2.579 2.579-2.579c1.424 0 2.579 1.154 2.579 2.578v0c0 0.001 0 0.002 0 0.004 0 1.423-1.154 2.577-2.577 2.577-0.001 0-0.002 0-0.003 0h0zM26.556 26.562h-4.441v-6.959c0-1.66-0.034-3.795-2.314-3.795-2.316 0-2.669 1.806-2.669 3.673v7.082h-4.441v-14.311h4.266v1.951h0.058c0.828-1.395 2.326-2.315 4.039-2.315 0.061 0 0.121 0.001 0.181 0.003l-0.009-0c4.5 0 5.332 2.962 5.332 6.817v7.855z"></path> </g></svg>
                     </a>
-                    <a href="/" className="h-8 w-8">
+                    <a href="jefflichen25@gmail.com" className="h-8 w-8">
                         <svg className="hover:fill-lime-500/75 hover:animate-pulse" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g><path d="M22,5V9L12,13,2,9V5A1,1,0,0,1,3,4H21A1,1,0,0,1,22,5ZM2,11.154V19a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V11.154l-10,4Z"></path></g></svg>
                     </a>
                 </ul>
 
             </div>
-            <div className="">
+            <div className="text-center sm:text-left sm:w-1/3">
                 <h1 className="text-2xl">
                     Hi, I'm Jeff
                 </h1>
-                <p className="h-1/2 sm:w-1/3 text-lg">
+                <p className="h-1/2 text-lg">
                     Sample text
+                    <br></br>
+                    This site's under construction
                 </p>
             </div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <nav className="w-3/4 h-20 flex flex-row text-xl text-neutral-800 items-center justify-between">
+        <nav className="w-3/4 h-20 flex flex-row text-lg text-neutral-800 items-center justify-center">
             <div className="hidden sm:block ">Jeff Chen</div>
             <div className="flex-grow-0 sm:flex-grow"></div>
             <a href="#home" className="px-2 hover:text-lime-500 hover:animate-pulse">Home</a>
