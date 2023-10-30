6ccb4ceefa904033adfb020166cd1cf41bc8c3eb
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-10-30 11:58:03 -0400

    set up react-spring

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,50 @@ import Home from "./pages/Home.tsx"
 import Projects from "./pages/Projects.tsx"
 import Contacts from "./pages/Contacts.tsx"
 import About from "./pages/About.tsx"
-// import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 
 function App() {
 
   return (
-    <div className="w-full h-screen overflow-y-scroll snap-y scroll-smooth">
+    <div className="w-screen h-screen ">
       <div className="fixed top-0 w-full flex justify-center p-2 z-50">
         <Navbar />
       </div>
-      <div className="h-screen">
+      {/* <div className="h-screen overflow-y-scroll snap-y scroll-smooth">
         <Home />
         <About />
         <Projects />
         <Contacts />
-      </div>
+      </div> */}
+      <Parallax pages={4} className="scroll-smooth">
+        <ParallaxLayer offset={0} className="">
+          <Home />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} className="">
+          <About />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} className="">
+          <Projects />
+        </ParallaxLayer>
+        <ParallaxLayer offset={3} className="">
+          <Contacts />
+        </ParallaxLayer>
+      </Parallax>
+      {/* <div className="w-screen h-screen absolute">
+        <Parallax pages={4}>
+          <ParallaxLayer
+            offset={1}
+            speed={5}
+            factor={1}
+            style={{
+              backgroundImage: `${bg10}`,
+              backgroundSize: 'cover',
+            }} 
+          >
+            <img src={bg10} />
+          </ParallaxLayer>
+        </Parallax>
+      </div> */}
     </div>
   )
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,13 +3,13 @@ const About = () => {
         <div id="about" className="relative h-full snap-start flex flex-col sm:flex-row justify-center items-center text-neutral-800">
             <div className="w-5/6 h-full flex flex-row items-center space-x-10">
                 <section className="h-3/4 w-full flex-grow rounded-xl shadow-xl bg-neutral-200/50 backdrop-blur-md p-8">
-                <h1 className="text-center text-lg text-neutral-800">
+                    <h1 className="text-center text-lg text-neutral-800">
                         Education
                     </h1>
                 </section>
 
                 <section className="h-3/4 w-full flex-grow rounded-xl shadow-xl bg-neutral-200/50 backdrop-blur-md p-8">
-                <h1 className="text-center text-lg text-neutral-800">
+                    <h1 className="text-center text-lg text-neutral-800">
                         Skills
                     </h1>
                 </section>
@@ -18,7 +18,6 @@ const About = () => {
                     <h1 className="text-center text-lg text-neutral-800">
                         Work Experience
                     </h1>
-
                 </section>
             </div>
         </div>
