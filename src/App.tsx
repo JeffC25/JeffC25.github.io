9d13c62248a4bf2df1ecb295be840c04e6d13a1a
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-11-04 14:07:06 -0400

    adjust layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,10 @@ import Home from "./pages/Home.tsx"
 import Projects from "./pages/Projects.tsx"
 import Contacts from "./pages/Contacts.tsx"
 import About from "./pages/About.tsx"
-import bg8 from "./assets/bg8.svg"
-
 function App() {
 
   return (
     <div className="w-full h-screen overflow-y-scroll snap-y scroll-smooth">
-    <img src={bg8} className="absolute top-0 object-cover h-full w-full -z-50" />
       <Navbar />
       <div className="h-screen">
         <Home />
@@ -17,7 +14,6 @@ function App() {
         <Projects />
         <Contacts />
       </div>
-      {/* <img src={bg8} className="absolute top-0 object-cover h-full w-full -z-50" /> */}
     </div>
   )
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,8 +9,8 @@ const About = () => {
         <div id="about" className="w-screen min-h-screen snap-start flex justify-center pt-44">
             <section className="h-2/3 w-full lg:w-1/2 px-10 flex flex-col">
                 <nav className="w-1/3 flex flex-row rounded-md backdrop-blur-md shadow-md bg-gradient-to-r from-cyan-500 to-lime-300">
-                    <button onClick={() => setTab(0)} className={`w-full hover:bg-transparent rounded-l-md py-4 ${(tab == 0 ? "bg-transparent" : "bg-neutral-200")}`}>About</button>{/* <button onClick={() => setTab(1)} className={`w-full hover:bg-transparent py-4 ${(tab == 1 ? "bg-transparent" : "bg-neutral-200")}`}>Experience</button> */}
-                    <button onClick={() => setTab(1)} className={`w-full hover:bg-transparent rounded-r-md py-4 ${(tab == 1 ? "bg-transparent" : "bg-neutral-200")}`}>Skills</button>
+                    <button onClick={() => setTab(0)} className={`w-full hover:bg-transparent rounded-l-md py-2 ${(tab == 0 ? "bg-transparent" : "bg-neutral-200")}`}>About</button>{/* <button onClick={() => setTab(1)} className={`w-full hover:bg-transparent py-4 ${(tab == 1 ? "bg-transparent" : "bg-neutral-200")}`}>Experience</button> */}
+                    <button onClick={() => setTab(1)} className={`w-full hover:bg-transparent rounded-r-md py-2 ${(tab == 1 ? "bg-transparent" : "bg-neutral-200")}`}>Skills</button>
                 </nav>
                 <AboutWidget tab={tab} />
                 <SkillsWidget tab={tab} />
