f38cc188f3cb6047042f2d0525848d42991210c5
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-11-04 01:38:58 -0400

    adjust about page height

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,5 +1,5 @@
 const Skill = (skill: string) => {
-    return <li className="inline bg-neutral-200/50 shadow-md rounded-md p-2 mr-2 mb-2">{skill}</li>
+    return <li className="bg-neutral-200/50 shadow-md rounded-md p-2 mr-2 mb-2">{skill}</li>
 }
 
 export default Skill
