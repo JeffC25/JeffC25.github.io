82fb74ba1ffa5c9cc14bcf9cb3a05c958c357e45
JeffC25 <jeff@yerf.dev>
2023-11-16 15:43:07 -0500

    apply home anchor to logo in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <nav className="fixed top-0 w-full h-16 flex flex-row text-lg items-center justify-center z-50 px-52  backdrop-blur-xl shadow-md">
-            <div className="hidden md:block ">Jeff Chen</div>
+            <a href="#home" className="hidden md:block ">Jeff Chen</a>
             <div className="flex-grow-0 md:flex-grow"></div>
             <a href="#home" className="text-center px-2 hover:text-lime-500 hover:animate-pulse">Home</a>
             <a href="#about" className="text-center px-2 hover:text-lime-500 hover:animate-pulse">About</a>
