dc6d5a1ee09cebcabd72ed6a01d7064e5ef83ce6
JeffC25 <jeff@yerf.dev>
2023-11-21 23:28:28 -0500

    adjust flex and rounding for projects page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -15,15 +15,15 @@ interface Props {
 
 const Project = ({title, description, website, image, alt, technologies, links}: Props) =>{
     return(
-        <section className="w-2/3 2xl:w-1/2 rounded-lg shadow-xl h-fit flex flex-col md:flex-row">
-            <a href={website} className="md:w-1/2 rounded-l-lg">
-                <img src={image} alt={alt} className="rounded-l-lg"/>
+        <section className="w-2/3 2xl:w-1/2 rounded-lg shadow-lg h-fit flex flex-col md:flex-row bg-neutral-100">
+            <a href={website} className="md:w-1/2 rounded-t-lg md:rounded-l-lg">
+                <img src={image} alt={alt} className="rounded-t-lg md:rounded-l-lg"/>
             </a>
-            <div className="md:w-1/2 p-2 bg-neutral-100 rounded-r-lg flex flex-col">
+            <div className="md:w-1/2 p-2  rounded-b-lg md:rounded-r-lg flex flex-col">
                 <div className="text-xl bg-clip flex flex-row space-x-1">
                     <h2 className="">{title}</h2>
                     <div className="flex-grow"></div>
-                    <div className="flex flex-row flex-wrap">
+                    <div className="flex flex-row flex-wrap justify-end">
                         {links.map((link) => (
                             <a href={link.url} className="border px-1 ml-1 h-min border-lime-400 hover:bg-lime-400 hover:animate-pulse rounded-md">{link.title}</a>
                         ))}
