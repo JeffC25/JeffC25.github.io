184abc65fe2981a0e511b7d0e05a522041a84acb
JeffC25 <jeff@yerf.dev>
2023-11-29 12:23:04 -0500

    update about page

--- src/components/SkillsWidget.tsx
+++ src/components/SkillsWidget.tsx
@@ -7,12 +7,12 @@ interface Props {
 
 const SkillsWidget = ({ tab }: Props) => {
     return (
-        <section className={`w-full pt-10 flex flex-col  ${(tab == 1 ? "block" : "hidden")}`}>
-            {SkillCategory({ category: "Languages", skills: ["Go", "C", "C++", "C#", "TypeScript", "JavaScript", "Python", "Java", "Lua", "HTML/CSS", "Verilog"] })}
-            {SkillCategory({ category: "Frameworks", skills: ["React", "Svelte", "Remix", "Express.js", "Flask", "Django", "ASP.net"] })}
+        <section className={`w-full pt-10 flex flex-col text-sm md:text-md ${(tab == 1 ? "block" : "hidden")}`}>
+            {SkillCategory({ category: "Languages", skills: ["Go", "C", "C++", "C#", "TypeScript", "JavaScript", "Python", "Java", "Lua", "HTML/CSS", "Verilog", "MATLAB"] })}
+            {SkillCategory({ category: "Frameworks", skills: ["React", "Svelte", "Remix", "Express.js", "Flask", "Django", "ASP.net", "Unity"] })}
             {SkillCategory({ category: "Databases", skills: ["PostgreSQL", "SQLite", "MongoDB", "Redis", "Neo4j", "DynamoDB"] })}
-            {SkillCategory({ category: "Tools", skills: ["Node.js", "Docker", "Kubernetes", "Git", "NGINX", "Amazon Web Services"] })}
-            {SkillCategory({ category: "Concepts", skills: ["Microservices", "REST APIs", "gRPC", "Object-Oriented Design", "Agile", "Unit Testing"]})}
+            {SkillCategory({ category: "Tools", skills: ["Node.js", "Docker", "Kubernetes", "Git", "NGINX", "Amazon Web Services", "Google Cloud"] })}
+            {SkillCategory({ category: "Concepts", skills: ["Microservices", "REST APIs", "gRPC", "Object-Oriented Design", "Agile", "Unit Testing"] })}
         </section>
     )
 }
