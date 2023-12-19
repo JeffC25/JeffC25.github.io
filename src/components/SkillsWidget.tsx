f4ee5ef2ea317b6bdfebd0f8f39b888f679faab7
JeffC25 <jeff@yerf.dev>
2023-12-18 22:13:10 -0500

    refactor functional renders to jsx rendering

--- src/components/SkillsWidget.tsx
+++ src/components/SkillsWidget.tsx
@@ -1,4 +1,3 @@
-// import Skill from "./Skill";
 import SkillCategory from "./SkillCategory";
 
 interface Props {
@@ -8,11 +7,11 @@ interface Props {
 const SkillsWidget = ({ tab }: Props) => {
     return (
         <section className={`w-full pt-10 flex flex-col text-sm md:text-md ${(tab == 1 ? "block" : "hidden")}`}>
-            {SkillCategory({ category: "Languages", skills: ["Go", "C", "C++", "C#", "TypeScript", "JavaScript", "Python", "Java", "Lua", "HTML/CSS", "Verilog", "MATLAB"] })}
-            {SkillCategory({ category: "Frameworks", skills: ["React", "Svelte", "Remix", "Express.js", "Flask", "Django", "ASP.net", "Unity"] })}
-            {SkillCategory({ category: "Databases", skills: ["PostgreSQL", "SQLite", "MongoDB", "Redis", "Neo4j", "DynamoDB"] })}
-            {SkillCategory({ category: "Tools", skills: ["Node.js", "Docker", "Kubernetes", "Git", "NGINX", "Amazon Web Services", "Google Cloud"] })}
-            {SkillCategory({ category: "Concepts", skills: ["Microservices", "REST APIs", "gRPC", "Object-Oriented Design", "Agile", "Unit Testing"] })}
+            <SkillCategory category="Languages" skills={["Go", "C", "C++", "C#", "TypeScript", "JavaScript", "Python", "Java", "Lua", "HTML/CSS", "Verilog", "MATLAB"]} />
+            <SkillCategory category="Frameworks" skills={["React", "Svelte", "Remix", "Express.js", "Flask", "Django", "ASP.net", "Unity"]} />
+            <SkillCategory category="Databases" skills={["PostgreSQL", "SQLite", "MongoDB", "Redis", "Neo4j", "DynamoDB"]} />
+            <SkillCategory category="Tools" skills={["Node.js", "Docker", "Kubernetes", "Git", "NGINX", "Amazon Web Services", "Google Cloud"]} />
+            <SkillCategory category="Concepts" skills={["Microservices", "REST APIs", "gRPC", "Object-Oriented Design", "Agile", "Unit Testing"]} />
         </section>
     )
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -12,105 +12,78 @@ const Projects = () => {
     return (
         <div id="projects" className="w-full snap-start flex justify-center flex-col items-center space-y-10 py-24">
             <h1 className="text-xl">Projects</h1>
-            {Project({
-                title: "Fridge Tracker",
-                description: "A touchscreen attachment for tracking your fridge contents with automated receipt scanning, recipe suggestions, expiration date alerts, and mobile device syncing.",
-                website: "#",
-                image: seniordesign,
-                alt: "Project Thumbnail",
-                technologies: ["React", "React Native", "TypeScript", "Python", "DynamoDB"],
-                links: [{ title: "In Progress", url: "#" }],
-            })}
-            {Project({
-                title: "Movie Browser",
-                description: "A fullstack website for browsing and discovering movies content, including overviews, reviews, cast, and trailers, as well as currated lists for trending, currently playing, upcoming, and top-rated films.",
-                website: "https://movie.yerf.dev",
-                image: moviebrowser,
-                alt: "Project Thumbnail",
-                technologies: ["React", "TypeScript", "Tailwind", "Go", "Redis", "PostgreSQL", "NGINX"],
-                links: [{ title: "Demo", url: "https://movie.yerf.dev" }, { title: "GitHub", url: "https://github.com/JeffC25/movie-browser" }]
-            })}
-
-            {Project({
-                title: "Chat App",
-                description: "A minimalist chat application with live messaging and private chatrooms.",
-                website: "https://chat-app.yerf.dev",
-                image: chatapp,
-                alt: "Project Thumbnail",
-                technologies: ["React", "JavaScript", "Tailwind", "Firebase"],
-                links: [{ title: "Demo", url: "https://chat-app.yerf.dev" }, { title: "GitHub", url: "https://github.com/JeffC25/chat-app" }]
-            })}
-
-            {Project({
-                title: "Smart Article Analyzer",
-                description: "An application to generate summaries, keywords, and sentiment from articles, PDF documents, and arbritrary text.",
-                website: "https://github.com/JeffC25/smart-article-analyzer",
-                image: smartarticleanalyzer,
-                alt: "Project Thumbnail",
-                technologies: ["Python", "Flask", "Bootstrap"],
-                links: [{ title: "GitHub", url: "https://github.com/JeffC25/smart-article-analyzer" },
-                ]
-            })}
-
-            {Project({
-                title: "Cookietective",
-                description: "An investigation of cookie leakage vulnerabilities due to CNAME cloaking across over 17,000 domains.",
-                website: "https://github.com/Cybersecurity-Project-Group/Cookietective",
-                image: cookietective,
-                alt: "Project Thumbnail",
-                technologies: ["Python", "Selenium", "SQLite", "Mitmproxy"],
-                links: [{ title: "GitHub", url: "https://github.com/Cybersecurity-Project-Group/Cookietective" }]
-            })}
-
-            {Project({
-                title: "BU Crossing",
-                description: "A Crossy Road-style game with custom Boston University-themed models.",
-                website: "https://github.com/JeffC25/BU-Crossing",
-                image: bucrossing,
-                alt: "Project Thumbnail",
-                technologies: ["C#", "Unity", "MagicaVoxel"],
-                links: [{ title: "GitHub", url: "https://github.com/JeffC25/BU-Crossing" }]
-            })}
-
-            {Project({
-                title: "Virtual File System",
-                description: "A lightweight Linux virtual file system library based on the File Allocation Table.",
-                website: "https://github.com/JeffC25/virtual-file-system",
-                image: virtualfilesystem,
-                alt: "Project Thumbnail",
-                technologies: ["C"],
-                links: [{ title: "GitHub", url: "https://github.com/JeffC25/virtual-file-system" }]
-            })}
-
-            {/* {Project({
-                title: "Inventory and Sales Monitor",
-                description: "A .NET application for inventory management and transaction tracking, with automated backups via Google Sheets.",
-                website: "#",
-                image: "#",
-                alt: "Project Thumbnail",
-                technologies: ["C#", "WPF", "SQLite"],
-                links: [{ title: "GitHub", url: "#" }]
-            })} */}
-
-            {/* {Project({
-                title: "MIPS CPU",
-                description: "A 32-bit MIPS CPU implemented in Verilog with control flow, hazard detection, and forwarding.",
-                website: "https://github.com/JeffC25/MIPS-CPU",
-                image: "#",
-                alt: "Project Thumbnail",
-                technologies: ["Verilog"],
-                links: [{ title: "GitHub", url: "https://github.com/JeffC25/MIPS-CPU" }]
-            })} */}
-
-            {Project({
-                title: "Personal Portfolio",
-                description: "This website!",
-                website: "https://github.com/JeffC25/JeffC25.github.io",
-                image: portfolio,
-                alt: "Project Thumbnail",
-                technologies: ["React", "TypeScript", "Tailwind"],
-                links: [{ title: "GitHub", url: "https://github.com/JeffC25/JeffC25.github.io" }]
-            })}
+            <Project
+                title="Fridge Tracker"
+                description="A touchscreen attachment for tracking your fridge contents with automated receipt scanning, recipe suggestions, expiration date alerts, and mobile device syncing."
+                website="#"
+                image={seniordesign}
+                alt="Project Thumbnail"
+                technologies={["React", "React Native", "TypeScript", "Python", "DynamoDB"]}
+                links={[{ title: "In Progress", url: "#" }]}
+            />
+            <Project
+                title="Movie Browser"
+                description="A fullstack website for browsing and discovering movies content, including overviews, reviews, cast, and trailers, as well as currated lists for trending, currently playing, upcoming, and top-rated films."
+                website="https://movie.yerf.dev"
+                image={moviebrowser}
+                alt="Project Thumbnail"
+                technologies={["React", "TypeScript", "Tailwind", "Go", "Redis", "PostgreSQL", "NGINX"]}
+                links={[{ title: "Demo", url: "https://movie.yerf.dev" }, { title: "GitHub", url: "https://github.com/jeffc25/movie-browser" }]}
+            />
+            <Project
+                title="Chat App"
+                description="A minimalist chat application with live messaging and private chatrooms."
+                website="https://chat-app.yerf.dev"
+                image={chatapp}
+                alt="Project Thumbnail"
+                technologies={["React", "JavaScript", "Tailwind", "Firebase"]}
+                links={[{ title: "Demo", url: "https://chat-app.yerf.dev" }, { title: "GitHub", url: "https://github.com/jeffc25/chat-app" }]}
+            />
+            <Project
+                title="Smart Article Analyzer"
+                description="An application to generate summaries, keywords, and sentiment from articles, PDF documents, and arbritrary text."
+                website="github.com/jeffc25/smart-article-analyzer"
+                image={smartarticleanalyzer}
+                alt="Project Thumbnail"
+                technologies={["Python", "Flask", "Bootstrap"]}
+                links={[{ title: "GitHub", url: "jeffc25/smart-article-analyzer" }]}
+            />
+            <Project
+                title="Cookietective"
+                description="An investigation of cookie leakage vulnerabilities due to CNAME cloaking across over 17,000 domains."
+                website="github.com/Cybersecurity-Project-Group/Cookietective"
+                image={cookietective}
+                alt="Project Thumbnail"
+                technologies={["Python", "Selenium", "SQLite", "Mitmproxy"]}
+                links={[{ title: "GitHub", url: "github.com/Cybersecurity-Project-Group/Cookietective" }]}
+            />
+            <Project
+                title="BU Crossing"
+                description="A Crossy Road-style game with custom Boston University-themed models."
+                website="github.com/jeffc25/BU-Crossing"
+                image={bucrossing}
+                alt="Project Thumbnail"
+                technologies={["C#", "Unity", "MagicaVoxel"]}
+                links={[{ title: "GitHub", url: "github.com/jeffc25/BU-Crossing" }]}
+            />
+            <Project
+                title="Virtual File System"
+                description="A lightweight Linux virtual file system library based on the File Allocation Table."
+                website="github.com/jeffc25/virtual-file-system"
+                image={virtualfilesystem}
+                alt="Project Thumbnail"
+                technologies={["C"]}
+                links={[{ title: "GitHub", url: "github.com/jeffc25/virtual-file-system" }]}
+            />
+            <Project
+                title="Personal Portfolio"
+                description="This website!"
+                website="github.com/jeffc25/JeffC25.github.io"
+                image={portfolio}
+                alt="Project Thumbnail"
+                technologies={["React", "TypeScript", "Tailwind"]}
+                links={[{ title: "GitHub", url: "github.com/jeffc25/JeffC25.github.io" }]}
+            />
         </div>
     )
 };
