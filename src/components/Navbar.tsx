c8fdb92996e0971f0c3b07efbd989583688c8191
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-02-20 00:19:03 -0500

    create scrolling util

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
+import scrollTo from "../utils/Scrolling";
+
 const Navbar = () => {
     return (
         <nav className="fixed top-0 w-full h-16 flex flex-row text-lg items-center justify-center z-50 px-20 lg:px-52  backdrop-blur-xl shadow-md">
-            <a href="#home" className="hidden md:block ">Jeff Chen</a>
+            <button onClick={() => scrollTo('home')} className="hidden md:block ">Jeff Chen</button>
             <div className="flex-grow-0 md:flex-grow"></div>
-            <a href="#home" className="text-center px-2 hover:text-lime-500 hover:animate-pulse">Home</a>
-            <a href="#about" className="text-center px-2 hover:text-lime-500 hover:animate-pulse">About</a>
-            <a href="#projects" className="text-center px-2 hover:text-lime-500 hover:animate-pulse">Projects</a>
-            <a href="#contacts" className="text-center px-2 hover:text-lime-500 hover:animate-pulse">Contacts</a>
+            <button onClick={() => scrollTo('home')} className="text-center px-2 hover:text-lime-500 hover:animate-pulse">Home</button>
+            <button onClick={() => scrollTo('about')} className="text-center px-2 hover:text-lime-500 hover:animate-pulse">About</button>
+            <button onClick={() => scrollTo('projects')} className="text-center px-2 hover:text-lime-500 hover:animate-pulse">Projects</button>
+            <button onClick={() => scrollTo('contacts')} className="text-center px-2 hover:text-lime-500 hover:animate-pulse">Contacts</button>
         </nav>
     )
 };
