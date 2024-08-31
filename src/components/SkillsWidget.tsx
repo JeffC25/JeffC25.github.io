8a2640795bd254af9578073434a637effe7c7113
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-08-31 11:25:23 -0400

    update SkillsWidget.tsx

--- src/components/SkillsWidget.tsx
+++ src/components/SkillsWidget.tsx
@@ -8,63 +8,67 @@ const SkillsWidget = ({ tab }: Props) => {
     return (
         <section className={`w-full pt-10 flex flex-col text-sm md:text-md ${(tab == 1 ? "block" : "hidden")}`}>
             <SkillCategory category="Languages" skills={[
-                "Go", 
-                "C", 
-                "C++", 
-                "TypeScript", 
-                "JavaScript", 
-                "Python", 
-                "Java", 
-                "C#", 
+                "Go",
+                "C",
+                "C++",
+                "TypeScript",
+                "JavaScript",
+                "Python",
+                "Java",
+                "C#",
                 "Zig",
-                "Ruby", 
-                "Lua", 
-                "HTML/CSS", 
+                "Ruby",
+                "Lua",
+                "HTML/CSS",
                 // "Verilog", 
                 // "MATLAB"
-                ]} />
+            ]} />
 
             <SkillCategory category="Frameworks" skills={[
-                "React", 
-                "Svelte", 
-                "Remix", 
-                "Express.js", 
-                "Flask", 
-                "Django", 
-                "PyTorch", 
-                "Spring Boot", 
-                "ASP.net", 
+                "React",
+                "Svelte",
+                "Remix",
+                "Express.js",
+                "Flask",
+                "Django",
+                "PyTorch",
+                "Spring Boot",
+                "ASP.net",
                 "Unity"
-                ]} />
+            ]} />
 
             <SkillCategory category="Databases" skills={[
-                "PostgreSQL", 
-                "SQLite", 
-                "MongoDB", 
-                "Redis", 
-                "Neo4j", 
+                "PostgreSQL",
+                "MySQL",
+                "Oracle DB",
+                "SQLite",
+                "MongoDB",
+                "Redis",
+                "Neo4j",
                 "DynamoDB"
-                ]} />
+            ]} />
 
             <SkillCategory category="Tools" skills={[
-                "Node.js", 
-                "Docker", 
-                "Kubernetes", 
-                "Git", 
-                "NGINX", 
-                "Amazon Web Services", 
-                "Google Cloud"
-                ]} />
+                "Node.js",
+                "Docker",
+                "Kubernetes",
+                "Git",
+                "gRPC",
+                "NGINX",
+                "Amazon Web Services",
+                "Google Cloud",
+                "CloudFlare"
+            ]} />
 
             <SkillCategory category="Concepts" skills={[
-                "Microservices", 
-                "Machine Learning", 
-                "REST APIs", 
-                "RPC", 
-                "Object-Oriented Design", 
-                "Agile", 
+                "Microservices",
+                // "Machine Learning", 
+                "REST APIs",
+                "RPC",
+                "Object-Oriented Design",
+                "Agile Methodology",
                 "CI/CD"
-                ]} />
+            ]} />
         </section>
     )
 }
